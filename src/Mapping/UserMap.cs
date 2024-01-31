97f4bb9db3f50a25ad9606e8e1f40d77d26d2726
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-01-30 21:07:23 -0300

    feat: create user map

---
+++ src/Mapping/UserMap.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using PortfolioAPI.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PortfolioAPI.Extensions;
+
+namespace PortfolioAPI.Mapping;
+
+public sealed class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ConfigureAuditProperties("users");
+
+        builder
+            .HasIndex(u => u.SpotifyAccountName)
+            .IsUnique();
+
+        builder
+            .Property(x => x.Name)
+            .HasColumnName("name")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder
+            .Property(x => x.FullName)
+            .HasColumnName("full_name")
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder
+            .Property(u => u.Position)
+            .HasColumnName("position")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder
+            .Property(u => u.Description)
+            .HasColumnName("description")
+            .HasMaxLength(1000)
+            .IsRequired();
+
+        builder
+            .Property(u => u.AvatarUrl)
+            .HasColumnName("avatar_url")
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder
+            .Property(u => u.SpotifyAccountName)
+            .HasColumnName("spotify_account_name")
+            .HasMaxLength(50);
+    }
+}
