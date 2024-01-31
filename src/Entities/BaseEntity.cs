c4281d3a732d2734bd4233a4f693eedc3be90df9
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-01-30 21:06:22 -0300

    feat: improve base entity

--- src/Entities/BaseEntity.cs
+++ src/Entities/BaseEntity.cs
@@ -1,6 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
 namespace PortfolioAPI.Entities;
 
 public abstract class BaseEntity
 {
-  public Guid Id { get; private set; }
+    [Column("id")]
+    [Key]
+    [NotNull]
+    [Required]
+    public Guid Id { get; private set; } = Guid.NewGuid();
+
+    [Column("created_at")]
+    [DataType(DataType.DateTime)]
+    [NotNull]
+    [Required]
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+    [Column("updated_at")]
+    [DataType(DataType.DateTime)]
+    public DateTime? UpdatedAt { get; private set; } = null;
+
+    [Column("excluded_at")]
+    [DataType(DataType.DateTime)]
+    public DateTime? ExcludedAt { get; private set; } = null;
 }
