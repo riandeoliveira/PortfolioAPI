7d433acfc4eb353473bd73818a8ba63b63fa60b4
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-01-31 13:15:07 -0300

    refactor: update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 using PortfolioAPI.Context;
 using Microsoft.EntityFrameworkCore;
+using PortfolioAPI.Repositories.Interfaces;
+using PortfolioAPI.Repositories;
+using PortfolioAPI.Services.Interfaces;
+using PortfolioAPI.Services;
+using PortfolioAPI.Entities;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// repositories
+builder.Services.AddScoped<IBaseRepository<User>, BaseRepository<User>>();
+
+// services
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
