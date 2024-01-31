8cdc736eeec1593210ba5e5505afb69015e8efe7
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-01-31 13:16:59 -0300

    feat: add 'CreateUserTable' migration

---
+++ src/Migrations/20240131145741_CreateUserTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioAPI.src.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    full_name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    position = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    description = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false),
+                    avatar_url = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    spotify_account_name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    excluded_at = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_spotify_account_name",
+                table: "users",
+                column: "spotify_account_name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
