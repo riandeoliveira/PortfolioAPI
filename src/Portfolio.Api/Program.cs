642489f72b1c2cfee8e49c8ab814141525766652
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-02-26 23:13:28 -0300

    docs: add auth in swagger docs

--- src/Portfolio.Api/Program.cs
+++ src/Portfolio.Api/Program.cs
@@ -2,6 +2,7 @@ using Serilog;
 using Portfolio.Entities.Context;
 using Microsoft.EntityFrameworkCore;
 using Portfolio.Api.Configurations;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,34 @@ builder.Services
     .ConfigureControllers()
     .ConfigureDependencies()
     .ConfigureDocumentation();
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddProblemDetails();
