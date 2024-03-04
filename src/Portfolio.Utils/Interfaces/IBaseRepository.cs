f723f986f3bc5834e49785534f04144010b42ea5
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-03 21:15:20 -0300

    feat: add new method to base repository

--- src/Portfolio.Utils/Interfaces/IBaseRepository.cs
+++ src/Portfolio.Utils/Interfaces/IBaseRepository.cs
@@ -10,9 +10,11 @@ public interface IBaseRepository<TEntity> where TEntity : BaseEntity
 
     Task<bool> ExistAsync(Guid id, CancellationToken cancellationToken = default);
 
-    Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    Task<bool> ExistAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
 
     Task<TEntity?> FindAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
 
     Task Remove(TEntity? entity, CancellationToken cancellationToken = default);
 
--- src/Portfolio.Utils/Repositories/BaseRepository.cs
+++ src/Portfolio.Utils/Repositories/BaseRepository.cs
@@ -28,14 +28,21 @@ public class BaseRepository<TEntity>(DatabaseContext context) : IBaseRepository<
         return entity is not null;
     }
 
-    public async Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    public async Task<bool> ExistAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
     {
-        return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate, cancellationToken);
+        var entity = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate, cancellationToken);
+
+        return entity is not null;
     }
 
     public async Task<TEntity?> FindAsync(Guid id, CancellationToken cancellationToken = default)
     {
         return await _context.Set<TEntity>().FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+    }
+
+    public async Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate, cancellationToken);
     }
 
     public async Task Remove(TEntity? entity, CancellationToken cancellationToken = default)
