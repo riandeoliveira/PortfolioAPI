3714c78279fe4c07d8d4d263781eb59e49572bdf
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-09 21:54:24 -0300

    feat: improve dependencies configuration

--- src/Portfolio.Api/Configurations/DependenciesConfiguration.cs
+++ src/Portfolio.Api/Configurations/DependenciesConfiguration.cs
@@ -1,7 +1,9 @@
 using Portfolio.Authors.Interfaces;
 using Portfolio.Authors.Repositories;
+using Portfolio.Authors.Validators;
 using Portfolio.Users.Interfaces;
 using Portfolio.Users.Repositories;
+using Portfolio.Users.Validators;
 using Portfolio.Utils.Interfaces;
 using Portfolio.Utils.Services;
 
@@ -12,10 +14,22 @@ public static class DependenciesConfiguration
     public static WebApplicationBuilder ConfigureDependencies(this WebApplicationBuilder builder)
     {
         builder.Services
+            // Repositories
             .AddScoped<IAuthorRepository, AuthorRepository>()
+            .AddScoped<IUserRepository, UserRepository>()
+
+            // Services
             .AddScoped<IAuthService, AuthService>()
             .AddScoped<ILocalizationService, LocalizationService>()
-            .AddScoped<IUserRepository, UserRepository>()
+
+            // Validators
+            .AddScoped<CreateAuthorValidator>()
+            .AddScoped<RemoveAuthorValidator>()
+            .AddScoped<SignInUserValidator>()
+            .AddScoped<SignUpUserValidator>()
+            .AddScoped<UpdateAuthorValidator>()
+
+            // Others
             .AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         return builder;
