b38f12faa1fc9319b7b5862fddc136a62a4ccd7f
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-11 23:23:12 -0300

    refactor: modularize author controller

--- src/Portfolio.Authors/Controllers/AuthorController.cs
+++ src/Portfolio.Authors/Controllers/AuthorController.cs
@@ -1,14 +1,7 @@
-using System.Net;
-
 using MediatR;
 
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
-using Portfolio.Authors.Features.Create;
-using Portfolio.Authors.Features.Remove;
-using Portfolio.Authors.Features.Update;
 
 namespace Portfolio.Authors.Controllers;
 
@@ -16,62 +9,6 @@ namespace Portfolio.Authors.Controllers;
 [Authorize]
 [Produces("application/json")]
 [Route("api/author")]
-public sealed class AuthorController(IMediator mediator) : ControllerBase
+public sealed partial class AuthorController(IMediator mediator) : ControllerBase
 {
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> CreateAsync([FromBody] CreateAuthorRequest request, CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            CreateAuthorResponse response = await mediator.Send(request, cancellationToken);
-
-            return StatusCode((int) HttpStatusCode.Created, response);
-        }
-        catch (Exception exception)
-        {
-            return BadRequest(exception.Message);
-        }
-    }
-
-    [HttpDelete("{id}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> RemoveAsync([FromRoute] Guid id, CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            await mediator.Send(new RemoveAuthorRequest(id), cancellationToken);
-
-            return NoContent();
-        }
-        catch (Exception exception)
-        {
-            return BadRequest(exception.Message);
-        }
-    }
-
-    [HttpPut]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> UpdateAsync([FromBody] UpdateAuthorRequest request, CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            await mediator.Send(request, cancellationToken);
-
-            return Ok();
-        }
-        catch (Exception exception)
-        {
-            return BadRequest(exception.Message);
-        }
-    }
 }
