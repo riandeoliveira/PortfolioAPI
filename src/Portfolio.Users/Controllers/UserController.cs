ad8618dfb98e4338c5da800a608df42cd948e9fd
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-11 23:23:40 -0300

    refactor: modularize user controller

--- src/Portfolio.Users/Controllers/UserController.cs
+++ src/Portfolio.Users/Controllers/UserController.cs
@@ -1,58 +1,17 @@
-using System.Net;
-
 using MediatR;
 
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-using Portfolio.Users.Features.SignIn;
-using Portfolio.Users.Features.SignUp;
 
 namespace Portfolio.Users.Controllers;
 
 [ApiController]
 [Produces("application/json")]
 [Route("api/user")]
-public sealed class UserController(IMediator mediator) : ControllerBase
+public sealed partial class UserController(IMediator mediator) : ControllerBase
 {
     // NOTE: RemoveAsync -> excluir conta
     // NOTE: SignInAsync -> iniciar sessão
     // NOTE: SignOutAsync -> encerrar sessão
     // NOTE: SignUpAsync -> cadastrar usuário
-
-    [HttpPost("sign-in")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> SignInAsync([FromBody] SignInUserRequest request, CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            SignInUserResponse response = await mediator.Send(request, cancellationToken);
-
-            return StatusCode((int) HttpStatusCode.OK, response);
-        }
-        catch (Exception exception)
-        {
-            return BadRequest(exception.Message);
-        }
-    }
-
-    [HttpPost("sign-up")]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> SignUpAsync([FromBody] SignUpUserRequest request, CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            SignUpUserResponse response = await mediator.Send(request, cancellationToken);
-
-            return StatusCode((int) HttpStatusCode.Created, response);
-        }
-        catch (Exception exception)
-        {
-            return BadRequest(exception.Message);
-        }
-    }
 }
