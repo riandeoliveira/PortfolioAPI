4862bb485a756ac4a6eee2a31ceec0edfe275dd0
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-28 14:34:13 -0300

    refactor: abstract AuthorController in separate files

---
+++ src/Portfolio.Application/UseCases/CreateAuthor/CreateAuthorEndpoint.cs
@@ -0,0 +1,48 @@
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Portfolio.Application.Endpoints;
+using Portfolio.Domain.Dtos;
+
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Portfolio.Application.UseCases.CreateAuthor
+{
+    /// <summary>
+    /// Endpoint for creating a new author.
+    /// </summary>
+    public sealed class CreateAuthorEndpoint(IMediator mediator) : AuthorEndpoint
+    {
+        /// <summary>
+        /// Handles the request to create a new author.
+        /// </summary>
+        /// <param name="request">The request containing the author's details.</param>
+        /// <param name="cancellationToken">A token to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(AuthorDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+        [SwaggerOperation(
+            Description = "Creates a new author and returns the author's details.",
+            OperationId = "CreateAuthor",
+            Tags = ["Author"]
+        )]
+        public async Task<IActionResult> Handle([FromBody] CreateAuthorRequest request, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                CreateAuthorResponse response = await mediator.Send(request, cancellationToken);
+
+                return StatusCode(StatusCodes.Status201Created, response.Author);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
---
+++ src/Portfolio.Application/UseCases/FindOneAuthor/FindOneAuthorEndpoint.cs
@@ -0,0 +1,48 @@
+using MediatR;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Portfolio.Application.Endpoints;
+using Portfolio.Domain.Dtos;
+
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Portfolio.Application.UseCases.FindOneAuthor
+{
+    /// <summary>
+    /// Endpoint for finding a single author by ID.
+    /// </summary>
+    public sealed class FindOneAuthorEndpoint(IMediator mediator) : AuthorEndpoint
+    {
+        /// <summary>
+        /// Handles the request to find a single author by ID.
+        /// </summary>
+        /// <param name="id">The ID of the author to find.</param>
+        /// <param name="cancellationToken">A token to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthorDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+        [SwaggerOperation(
+            Description = "Finds a single author by ID and returns their details.",
+            OperationId = "FindOneAuthor",
+            Tags = ["Author"]
+        )]
+        public async Task<IActionResult> Handle([FromRoute] Guid id, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                FindOneAuthorResponse response = await mediator.Send(new FindOneAuthorRequest(id), cancellationToken);
+
+                return Ok(response.Author);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
