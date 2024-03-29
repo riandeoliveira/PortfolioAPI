908ddae86a3b00852429aaf88e0b3c502301f187
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-03-29 20:44:15 -0300

    feat: create ValidationBehavior

--- src/Portfolio.WebApi/Extensions/ControllersExtension.cs
+++ src/Portfolio.WebApi/Extensions/ControllersExtension.cs
@@ -1,5 +1,12 @@
 using System.Text.Json;
 
+using FluentValidation;
+
+using MediatR;
+
+using Portfolio.Application.Endpoints;
+using Portfolio.Infrastructure.Behaviors;
+
 using Serilog;
 
 namespace Portfolio.WebApi.Extensions;
@@ -9,6 +16,8 @@ internal static class ControllerExtension
     internal static WebApplicationBuilder ConfigureControllers(this WebApplicationBuilder builder)
     {
         builder.Services
+            .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
+            .AddValidatorsFromAssemblyContaining<BaseEndpoint>()
             .AddProblemDetails()
             .AddControllersWithViews()
             .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower);
